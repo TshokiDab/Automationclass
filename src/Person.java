5241caa10c852e2773aaa96a8114ff0788855dc1
boitshokodabula@gmail.com <Bontleeng@29Mash>
2023-03-08 16:13:47 +0200

    created class person with objects

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,11 @@
 public class Person {
+
+    public static void main(String[] args) {
+
+        String firstName = "Tshoki";
+        String lastName = "Dabula";
+        int age = 20;
+
+        System.out.println("Name: "+ firstName + " " + lastName + " " +age);
+    }
 }
